9da2292e7d560a98f6a88f4e5882927bb20b05e3
Sameera <sameerawncd@gmail.com>
2023-05-29 18:16:50 +0530

    revamp with websocket

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerWSHandler.java
@@ -0,0 +1,44 @@
+package lk.ijse.dep10.pos.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lk.ijse.dep10.pos.dto.CustomerDTO;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class CustomerWSHandler extends TextWebSocketHandler {
+
+    @Autowired
+    private BasicDataSource pool;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Customer WHERE id=? OR contact=?");
+            stm.setString(1, message.getPayload());
+            stm.setString(2, message.getPayload());
+            ResultSet rst = stm.executeQuery();
+            if (rst.next()) {
+                int id = rst.getInt("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                String contact = rst.getString("contact");
+                CustomerDTO customer = new CustomerDTO(id, name, address, contact);
+                String customerJSON = objectMapper.writeValueAsString(customer);
+                session.sendMessage(new TextMessage(customerJSON));
+            }
+        }
+    }
+}
