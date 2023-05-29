088d3b4ba16e5a991f754c4485bd5421f88dc00e
Sameera <sameerawncd@gmail.com>
2023-05-29 14:22:50 +0530

    expose a new service to retrieve a customer

--- front-end/js/place-order.js
+++ front-end/js/place-order.js
@@ -1,6 +1,15 @@
-for(let i = 0; i < 15; i++){
-    const tr = $("#tbl-order tbody tr:first-child").clone();
-    $("#tbl-order tbody").append(tr);
+import {LocalDateTime, DateTimeFormatter} from "../node_modules/@js-joda/core/dist/js-joda.esm.js";
+
+const orderDatetime = $("#order-date-time");
+const tbodyElm = $("#tbl-order tbody");
+
+setDateTime();
+tbodyElm.empty();
+
+setInterval(setDateTime,1000);
+function setDateTime(){
+    const now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    orderDatetime.text(now);
 }
 
-$("#tbl-order tbody").empty();
+
