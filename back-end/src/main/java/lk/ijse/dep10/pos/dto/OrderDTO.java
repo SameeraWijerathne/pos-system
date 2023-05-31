3e4c96f58f0057490c8edd94ac91a602ea910ad1
Sameera <sameerawncd@gmail.com>
2023-05-31 12:58:21 +0530

    finalize the save order web service

--- back-end/src/main/java/lk/ijse/dep10/pos/dto/OrderDTO.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/dto/OrderDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep10.pos.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class OrderDTO {
     private CustomerDTO customer;
-    private LocalDateTime localDateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateTime;
     private List<ItemDTO> itemList;
 }
