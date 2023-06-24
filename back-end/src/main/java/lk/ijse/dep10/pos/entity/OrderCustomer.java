d94eb530c6daf53ac8d478e8479dde06515cab2e
Sameera <sameerawncd@gmail.com>
2023-06-24 17:38:05 +0530

    started applying layered architecture (MVC2) to the back end - DAO layer implemeted

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/entity/OrderCustomer.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.pos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCustomer implements SuperEntity {
+    private int orderId;
+    private int customerId;
+}
